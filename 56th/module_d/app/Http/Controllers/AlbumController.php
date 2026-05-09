3f85cba9a20ad247fc79f64fbadc0d6f6cf29947
Kortez <kurata9527@gmail.com>
2026-05-10 03:30:37 +0800

    56th : implement Album resource for index api

---
+++ 56th/module_d/app/Http/Controllers/AlbumController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Album;
+use App\Http\Resources\AlbumResource;
+use Illuminate\Http\Request;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        // 取得所有專輯並分頁
+        $albums = Album::cursorPaginate($request->query('limit', 10));
+
+        // 包裝成 Resource Collection
+        return AlbumResource::collection($albums)->additional([
+            'success' => true,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Album $album)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Album $album)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Album $album)
+    {
+        //
+    }
+}
--- 56th/module_d/routes/api.php
+++ 56th/module_d/routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AlbumController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('albums')->group(function () {
+    // 當訪問 /api/albums 時執行 index
+    Route::get('/', [AlbumController::class, 'index']);
+});
